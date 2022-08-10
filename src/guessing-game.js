27e574ecee3f8c469ea824f2fb1b77ee5665872b
Viktoryia Shevchenko <shevviktoryia@gmail.com>
2022-08-10 14:48:06 +0300

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,38 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
     }
 
-    guess() {
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
 
+
+    guess() {
+        if(this.min != this.max) {
+            return this.middle = Math.ceil((this.min + this.max) / 2);
+        }
+        else {
+            return this.middle = Math.floor((this.min + this.max) / 2);
+        }
     }
 
     lower() {
-
+        if(this.min < this.max) {
+            this.max = this.middle;
+        }
+        else {
+            this.max = this.middle - 2;
+        }
     }
 
     greater() {
-
+        if(this.min != this.max) {
+            this.min = this.middle;
+        }
+        else {
+            this.min = this.middle + 1;
+        }
     }
 }
 
